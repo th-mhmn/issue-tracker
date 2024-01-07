8466d39f1e873754d7257ed8f3768a2499e9f6f1
taha <tahamehmandoust@gmail.com>
2024-01-07 11:23:45 +0330

    handle error

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,36 +1,49 @@
 'use client'
-import { Button, TextField } from '@radix-ui/themes'
+import { Button, TextField, Callout, CalloutText } from '@radix-ui/themes'
 import SimpleMDE from 'react-simplemde-editor'
 import 'easymde/dist/easymde.min.css'
 import { useForm, Controller } from 'react-hook-form'
 import axios from 'axios'
 import { IssueForm } from '@/utils/interfaces'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 const NewIssuePage = () => {
   const { register, control, handleSubmit } = useForm<IssueForm>()
   const router = useRouter()
+  const [error, setError] = useState('')
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/issues', data)
-        router.push('/issues')
-      })}
-    >
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register('title')} />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-      <Button>Submit New Issue</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <CalloutText>{error}</CalloutText>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post('/api/issues', data)
+            router.push('/issues')
+          } catch (error) {
+            setError('An unexpected error occured.')
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register('title')} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   )
 }
 
--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,6 +1,9 @@
 import { z } from 'zod'
 
 export const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z
+    .string()
+    .min(1, 'Title is required.')
+    .max(255, 'Title must have maximum of 255 characters.'),
+  description: z.string().min(1, 'Description is required.'),
 })
