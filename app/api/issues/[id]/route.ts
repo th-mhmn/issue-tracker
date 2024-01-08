dc93b53d52566fd11c0f9eacaae91912cfabe5bb
taha <tahamehmandoust@gmail.com>
2024-01-08 15:33:40 +0330

    build api for deleting issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -7,7 +7,6 @@ export async function PATCH(
   { params }: { params: { id: string } },
 ) {
   const body = await request.json()
-
   const validation = issueSchema.safeParse(body)
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 })
@@ -27,4 +26,26 @@ export async function PATCH(
   })
 
   return NextResponse.json(updatedIssue)
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  })
+  if (!issue)
+    return NextResponse.json(
+      { error: 'Issue with the given id not found' },
+      { status: 404 },
+    )
+
+  await prisma.issue.delete({
+    where: { id: parseInt(params.id) },
+  })
+
+  return NextResponse.json({})
 }
