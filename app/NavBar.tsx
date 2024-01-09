074370df611d7bd21ac918bf87d17dd3725bea08
taha <tahamehmandoust@gmail.com>
2024-01-09 10:45:25 +0330

    fix avatar not loading

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -58,6 +58,7 @@ const NavBar = () => {
                     size="2"
                     src={session.user!.image!}
                     fallback="?"
+                    referrerPolicy="no-referrer"
                   />
                 </DropdownMenu.Trigger>
                 <DropdownMenu.Content>
